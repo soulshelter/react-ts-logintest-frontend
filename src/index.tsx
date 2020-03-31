d8ae3a5b4cd56154165dce76ebf4c8cc118c3bea
auroville <auroville.pms@gmail.com>
2020-03-31 10:38:08 +0900

    Add : redux 모듈 설치

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from './serviceWorker';
 import Routes from "./routes"
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './modules';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
-  <Routes />,
+  <Provider store={store}>
+    <Routes />
+  </Provider>,
   document.getElementById("root")
 );
 
